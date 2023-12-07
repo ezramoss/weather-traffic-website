3a216b36c4ffc1a127eeb4d1e66f9a1a6098cfd7
Ezra <ezramoss4@gmail.com>
2023-12-06 17:25:04 -0800

    Moved querydata to own function, refactored weather card ui, removed global measurement

--- src/App.js
+++ src/App.js
@@ -2,64 +2,31 @@ import "./App.css";
 import { Button, Card, Form, Row, Col, Container, ButtonGroup, ToggleButton, Image,} from "react-bootstrap";
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import axios from "axios";
-import Day from "./Day.js";
-
-var globalLocations = new Array(3)
-var globalMeasurement = 1
+import QueryData from "./QueryData.js";
 
 function WeatherWebsite() {
   const [measurement, setMeasurement] = useState(1);
   const [days, setDays] = useState([]);
   const [formData, setFormData] = useState("");
   const [locations, setLocation] = useState([])
-  const [newQuery, setQuery] = useState(1)
   const [isChecked, setChecked] = useState("1");
-  const measurements = [
-    { name: "°F", value: "1" },
-    { name: "°C", value: "2" },
-  ];
+  const measurements = [ { name: "°F", value: "1" }, { name: "°C", value: "2" }];
   var cityQuery = formData
 
   //Change Between Celsius and Fahrenheit
   function changeMeasurement() {
-    return measurement === 1 ? (globalMeasurement = 2, setMeasurement(2)):
-      (globalMeasurement = 1, setMeasurement(1));
+    return measurement === 1 ? setMeasurement(2): setMeasurement(1);
   }
 
   //Query Backend & Populate Temp/Condition Arrays with Location Result
+  async function setData(){
+    let {tempDays, locationData} = await QueryData(cityQuery);
+    setDays(tempDays)
+    setLocation(locationData)
+  }
+
   useEffect(() => {
-    if(formData === "") return
-      axios.get("https://weather-website-backend-0be487779640.herokuapp.com/getWeather/" + cityQuery).then((response) => {
-      const tempDays = []
-      console.log(response)
-      for(let i = 0;i < response.data.dayArray.length;i++) {
-        var date = new Date(response.data.dayArray[i]['date'])
-        date.setDate(date.getDate() + 1)
-        date = date.toLocaleDateString('en-US', { weekday: 'long' })
-        const newDay = new Day(
-          response.data.dayArray[i]['maxTempC'],
-          response.data.dayArray[i]['minTempC'],
-          response.data.dayArray[i]['maxTempF'],
-          response.data.dayArray[i]['minTempF'],
-          response.data.dayArray[i]['conditions'],
-          response.data.dayArray[i]['icon'],
-          date
-        )
-        tempDays.push(newDay)
-      }
-      setDays(tempDays)
-
-      //Get & Set Location
-      globalLocations[0] = response.data.name
-      globalLocations[1] = response.data.region
-      globalLocations[2] = response.data.country
-      setLocation(globalLocations);
-
-      //Update React to Re-Render page
-      newQuery === 1 ? setQuery(2) : setQuery(1)
-    });
-  
+    setData()
   }, [formData]);
 
   const onFormSubmit = (event) => {
@@ -121,16 +88,12 @@ function WeatherWebsite() {
         {/* Weather Cards */}
 
         <Container className="mt-5">
-          {formData &&
-          <div>
             <Row className="justify-content-center" xs="auto">
-              <h1>{locations[0]}, {locations[1]}, {locations[2]}</h1>
+              <h1>{locations[0] === undefined ? "" : locations[0] + ", " + locations[1] + ", " + locations[2]}</h1>
             </Row>
             <Row className="justify-content-center">
-              {days.map((day) => <WeatherCard day = {day}/>)}
+              {days.map((day) => <WeatherCard day = {day} measurement = {measurement}/>)}
             </Row>
-          </div>
-          }
         </Container>
         <Card.Footer className="custom-footer" style={{ backgroundColor: "grey" }}>
           <p>This website is a passion project and is not intended for detailed weather reports. Please direct any questions, comments, or concerns to ezramoss4@gmail.com</p>
@@ -153,8 +116,8 @@ function WeatherCard(props) {
       </div>
       <Card.Body>
           <h3>{props.day.conditions}</h3>
-          <h3>High: {globalMeasurement === 1 ? props.day.maxTempF :  props.day.maxTempC }</h3>
-          <h3>Low: {globalMeasurement === 1 ? props.day.minTempF :  props.day.minTempC }</h3>
+          <h3>High: {props.measurement === 1 ? props.day.maxTempF :  props.day.maxTempC }</h3>
+          <h3>Low: {props.measurement === 1 ? props.day.minTempF :  props.day.minTempC }</h3>
       </Card.Body>
     </Card>
   );
---
+++ src/QueryData.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import Day from "./Day.js";
+
+function QueryData (cityQuery) {
+    console.log("cityQuery: " + cityQuery)
+    let tempDays = new Array(3)
+    let locationData = []
+    if(cityQuery === "") return {tempDays, locationData }
+    //axios.get("https://weather-website-backend-0be487779640.herokuapp.com/getWeather/" + cityQuery).then((response) => {
+    return axios.get("http://localhost:5000/getWeather/" + cityQuery).then((response) => {
+      console.log(response)
+      for(let i = 0;i < response.data.dayArray.length;i++) {
+        var date = new Date(response.data.dayArray[i]['date'])
+        date.setDate(date.getDate() + 1)
+        date = date.toLocaleDateString('en-US', { weekday: 'long' })
+        const newDay = new Day(
+          response.data.dayArray[i]['maxTempC'],
+          response.data.dayArray[i]['minTempC'],
+          response.data.dayArray[i]['maxTempF'],
+          response.data.dayArray[i]['minTempF'],
+          response.data.dayArray[i]['conditions'],
+          response.data.dayArray[i]['icon'],
+          date
+        )
+        tempDays[i] = newDay
+      }
+      //Get & Set Location
+      locationData[0] = response.data.name
+      locationData[1] = response.data.region
+      locationData[2] = response.data.country
+      return {tempDays, locationData }
+    });
+}
+export default QueryData;
