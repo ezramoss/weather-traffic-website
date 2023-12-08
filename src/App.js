889f7fb5544ae93c1fea66e4cf8f858cbd794733
Ezra <ezramoss4@gmail.com>
2023-12-07 16:07:46 -0800

    test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import WeatherPage from "./WeatherPage.js";
 
 function WeatherWebsite() {
   //Main Content
+  console.log(process.env.REACT_APP_GOOGLE_KEY)
   return <WeatherPage></WeatherPage>
 }
 
